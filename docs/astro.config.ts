0f8fb4c8967ce55b3193c7cbc3a7194fa89827ff
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-10-26 20:44:00 +0100

    docs: add og image

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -2,6 +2,10 @@ import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
 import starlightGitHubAlerts from 'starlight-github-alerts'
 
+const site =
+  (process.env['CONTEXT'] === 'production' ? process.env['URL'] : process.env['DEPLOY_PRIME_URL']) ??
+  'https://starlight-github-alerts.netlify.app/'
+
 export default defineConfig({
   integrations: [
     starlight({
@@ -9,6 +13,16 @@ export default defineConfig({
       editLink: {
         baseUrl: 'https://github.com/HiDeoo/starlight-github-alerts/edit/main/docs/',
       },
+      head: [
+        {
+          tag: 'meta',
+          attrs: { property: 'og:image', content: new URL('og.jpg', site).href },
+        },
+        {
+          tag: 'meta',
+          attrs: { property: 'og:image:alt', content: 'Starlight plugin to render GitHub alerts as Starlight asides.' },
+        },
+      ],
       plugins: [starlightGitHubAlerts()],
       sidebar: [
         {
@@ -36,6 +50,6 @@ export default defineConfig({
       title: 'Starlight GitHub Alerts',
     }),
   ],
-  site: 'https://starlight-github-alerts.netlify.app/',
+  site,
   trailingSlash: 'always',
 })
