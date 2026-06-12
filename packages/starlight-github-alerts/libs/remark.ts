b86cfd5ca2acb26145e986a649a73ba3e7594aaa
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-06-12 14:49:51 +0200

    Add support for `markdown.processedDirs` (#9)

--- packages/starlight-github-alerts/libs/remark.ts
+++ packages/starlight-github-alerts/libs/remark.ts
@@ -3,24 +3,18 @@ import type { Plugin } from 'unified'
 import { CONTINUE, SKIP, visit } from 'unist-util-visit'
 
 import type { StarlightGitHubAlertsConfig } from './config'
+import { shouldTransformPath } from './starlight'
 
 const alertRegex = /^(?<line>\[!(?<type>\w+)][\r\n]*)/
 
 export const remarkStarlightGitHubAlerts: Plugin<[RemarkStarlightGitHubAlertsConfig], Root> = function ({
   config,
-  docsCollectionPath,
+  markdownProcessorPaths,
 }) {
-  const normalizedDocsCollectionPath = normalizePath(docsCollectionPath)
-
   return (tree, file) => {
     visit(tree, 'blockquote', (node, index, parent) => {
       if (!parent || index === undefined) return CONTINUE
-
-      // If the content does not have a path, e.g. when rendered using the content loader `renderMarkdown()` API, skip
-      // the file.
-      if (!file.path) return false
-      // If the document is not part of the Starlight docs collection, skip it.
-      if (!normalizePath(file.path).startsWith(normalizedDocsCollectionPath)) return CONTINUE
+      if (!shouldTransformPath(file.path, markdownProcessorPaths)) return false
 
       const [firstChild] = node.children
       if (firstChild?.type !== 'paragraph') return CONTINUE
@@ -56,15 +50,7 @@ function isValidType(
   return type !== undefined && type.toLowerCase() in config.types
 }
 
-/**
- * File path separators seems to be inconsistent on Windows between remark/rehype plugins used on Markdown vs MDX files.
- */
-const backSlashRegex = /\\/g
-function normalizePath(path: string) {
-  return path.replaceAll(backSlashRegex, '/')
-}
-
 interface RemarkStarlightGitHubAlertsConfig {
   config: StarlightGitHubAlertsConfig
-  docsCollectionPath: string
+  markdownProcessorPaths: string[]
 }
