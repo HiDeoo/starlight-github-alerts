7ef9d221de556f57cc8c1372f362b297d8a71c33
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-06-17 14:45:58 +0200

    feat: add support for starlight 0.40

--- packages/starlight-github-alerts/index.ts
+++ packages/starlight-github-alerts/index.ts
@@ -1,6 +1,7 @@
 import type { StarlightPlugin } from '@astrojs/starlight/types'
 import { AstroError } from 'astro/errors'
 
+import { isUnifiedProcessor } from './libs/astro'
 import { StarlightGitHubAlertsConfigSchema, type StarlightGitHubAlertsUserConfig } from './libs/config'
 import { remarkStarlightGitHubAlerts } from './libs/remark'
 import { getMarkdownProcessorPaths } from './libs/starlight'
@@ -9,9 +10,8 @@ export default function starlightGitHubAlerts(userConfig?: StarlightGitHubAlerts
   const parsedConfig = StarlightGitHubAlertsConfigSchema.safeParse(userConfig)
 
   if (!parsedConfig.success) {
-    throw new AstroError(
+    throwPluginError(
       `The provided plugin configuration is invalid.\n${parsedConfig.error.issues.map((issue) => issue.message).join('\n')}`,
-      `See the error report above for more informations.\n\nIf you believe this is a bug, please file an issue at https://github.com/HiDeoo/starlight-github-alerts/issues/new/choose`,
     )
   }
 
@@ -25,14 +25,23 @@ export default function starlightGitHubAlerts(userConfig?: StarlightGitHubAlerts
             'astro:config:setup': ({ command, config: astroConfig }) => {
               if (command !== 'dev' && command !== 'build') return
 
-              const remarkDirectiveIndex = astroConfig.markdown.remarkPlugins.findIndex(
+              const markdownProcessor = astroConfig.markdown.processor
+
+              if (!isUnifiedProcessor(markdownProcessor)) {
+                // When not using the Unified processor, we throw an error, since we don't support Sätteri yet.
+                throwPluginError(
+                  "The configured 'markdown.processor' is not supported. Switch to 'unified()' from '@astrojs/markdown-remark'.",
+                )
+              }
+
+              const remarkDirectiveIndex = markdownProcessor.options.remarkPlugins.findIndex(
                 (plugin) => typeof plugin === 'function' && plugin.name === 'remarkDirective',
               )
 
               if (remarkDirectiveIndex === -1) return
 
               // Inject the remark plugin before the `remarkDirective` plugin.
-              astroConfig.markdown.remarkPlugins.splice(remarkDirectiveIndex, 0, [
+              markdownProcessor.options.remarkPlugins.splice(remarkDirectiveIndex, 0, [
                 remarkStarlightGitHubAlerts,
                 {
                   config: parsedConfig.data,
@@ -45,4 +54,11 @@ export default function starlightGitHubAlerts(userConfig?: StarlightGitHubAlerts
       },
     },
   }
+}
+
+function throwPluginError(message: string): never {
+  throw new AstroError(
+    message,
+    `See the error report above for more information.\n\nIf you believe this is a bug, please file an issue at https://github.com/HiDeoo/starlight-github-alerts/issues/new/choose`,
+  )
 }
